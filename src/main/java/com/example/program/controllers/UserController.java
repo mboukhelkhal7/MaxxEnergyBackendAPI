4878aed6c19a105f1e3b68b6f3e14fb1703a60cc
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-08-05 11:26:58 -0700

    Update UserController.java

--- src/main/java/com/example/program/controllers/UserController.java
+++ src/main/java/com/example/program/controllers/UserController.java
@@ -1,4 +1,15 @@
 package com.example.program.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/dashboard")
+    public String userDashboard() {
+        return "Welcome, USER!";
+    }
 }
